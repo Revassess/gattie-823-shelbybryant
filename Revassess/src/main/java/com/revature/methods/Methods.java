4f4d256814b26ed76f066cc490aac39eaba8c2f4
shelbybryant <69205358+shelbybryant@users.noreply.github.com>
2020-08-26 14:33:40 -0400

    revasses tier1 version 1
    created user class with variables, getters/setters, constructor
    made the compare strings method

---
+++ Revassess/src/main/java/com/revature/methods/Methods.java
@@ -0,0 +1,11 @@
+package com.revature.methods;
+
+public class Methods {
+
+	public void checkEquality(String string1, String string2) {
+		if(string1.equals(string2)) {
+			System.out.println("The strings are equal");
+		}
+	}
+	
+}
--- Revassess/src/main/java/com/revature/tier1/User.java
+++ Revassess/src/main/java/com/revature/tier1/User.java
@@ -1,4 +1,45 @@
 package com.revature.tier1;
 
 public class User {
+	
+	private int id;
+	public String firstName;
+	public String lastName;
+	private String username;
+	private String password;
+	public String role;
+	
+	public User(int id, String firstName, String lastName, String username, String password, String role) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+		this.password = password;
+		this.role = role;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
