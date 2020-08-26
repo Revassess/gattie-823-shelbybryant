9ceefaa188688796e14d97e54d8e7358cd0a196c
shelbybryant <69205358+shelbybryant@users.noreply.github.com>
2020-08-26 15:27:48 -0400

    revassess tier1 version 4
    Fixed compare string method - had to actually create strings to compare
    completed the numbersumlength method?

--- Revassess/src/main/java/com/revature/tier1/CompareStrings.java
+++ Revassess/src/main/java/com/revature/tier1/CompareStrings.java
@@ -2,7 +2,11 @@ package com.revature.tier1;
 
 public class CompareStrings {
 
+	String s1 = "ABC";
+	String s2 = "abc";
+	
     public static boolean compareStrings(String s1, String s2){
+    	
     	if(s1.equals(s2)) {
 			return true;
 		}else {
--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -2,7 +2,26 @@ package com.revature.tier1;
 
 public class NumberSumLength {
 
+	
+	
 	public static boolean checkNumberPowerLength(long num) {
-		return false;
+		//the long num is 
+		int number = 5;
+		long overallLength = (int) (Math.log10(number) +1);
+		
+		int sum = 0;
+		int result = (int)Math.pow(number, overallLength);
+		while (result != 0) {
+			int n = result % 10;
+			sum += n;
+			result /= 10;
+		}
+		
+		if (sum == number) {
+			return true;
+		}else {
+			return false;
+		}
+		
 	}
 }
