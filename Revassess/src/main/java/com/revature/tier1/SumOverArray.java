d9fde44e524dbc3dfd5e6f6e3bb82090fc167a93
shelbybryant <69205358+shelbybryant@users.noreply.github.com>
2020-08-26 15:01:28 -0400

    revassess tier1 version 3
    finished method for the sum of an array
    going to push and see if it works

--- Revassess/src/main/java/com/revature/tier1/SumOverArray.java
+++ Revassess/src/main/java/com/revature/tier1/SumOverArray.java
@@ -1,8 +1,25 @@
 package com.revature.tier1;
 
+import java.util.stream.IntStream;
+
 public class SumOverArray {
 
 	public static int IterateAndSum(int[] arr) {
+		int[] array = new int[4];
+		arr[0] = 1;
+		arr[1] = 2;
+		arr[2] = 3;
+		arr[3] = 4;
+		
+		int sum = IntStream.of(array).sum();
+		
+		if(sum == 10) {
+			System.out.println("The sum of the array is " + sum);
+		} else {
+			System.out.println("There was an error calculating the sum of the array");
+		}
+		
+		
 		return 0;
 	}
 }
